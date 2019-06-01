2dd071f11538941a1f34cec9d8b1a3739ad4e514
Daniel <daniel.pavela@gmail.com>
2019-06-01 12:56:37 +1000

    Added Windows 10 Push notifications
    Signed-off-by: Daniel <daniel.pavela@gmail.com>

---
+++ notifications/windows_10_push_notifications.py
@@ -0,0 +1,24 @@
+from win10toast import ToastNotifier
+import time
+
+toaster = ToastNotifier()
+
+
+def notify(title, message):
+    toaster.show_toast(title,
+                       message,
+                       icon_path=None,
+                       duration=5,
+                       threaded=True)
+
+    # Wait for threaded notification to finish
+    while toaster.notification_active():
+        time.sleep(0.1)
+
+
+async def main():
+    print("0")
+    notify("Hoi", "lol")
+    print("1")
+    notify("Hoi", "lol")
+    print("2")

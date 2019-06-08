9ceafdf6adfb499677597c2685716595494b7eb4
Daniel <daniel.pavela@gmail.com>
2019-06-08 11:08:05 +1000

    Updated 7-segment-display to display all allowed words for the user.
    Signed-off-by: Daniel <daniel.pavela@gmail.com>

--- Number_Systems/7_segment_display.py
+++ Number_Systems/7_segment_display.py
@@ -4,6 +4,7 @@ words_document = open("all_words.txt", "r")
 all_raw_words = words_document.read()
 words_document.close()
 all_words = all_raw_words.split("\n")
+allowed_list = []
 
 highest_allowed_word = ""
 
@@ -20,23 +21,38 @@ letters_to_numbers = {
     "b": "8",
 }
 
+
+def convert_to_calculator_number(word_provided):
+    translated_numbers = []
+    for letter in word_provided:
+        translated_numbers.append(letters_to_numbers[letter.lower()])
+
+    translated_numbers.reverse()
+    translated_numbers = "".join(translated_numbers)
+    return translated_numbers
+
+
 for word in all_words:
-    if len(word) > len(highest_allowed_word):
-        if bool(re.match('^[oizehsglb]+$', word.lower())) is True:
+    if bool(re.match('^[oizehsglb]+$', word.lower())) is True:
+        if len(word) > len(highest_allowed_word):
             highest_allowed_word = word
-            print("{word} has passed".format(word=word))
-        else:
-            print("{word} has failed".format(word=word))
+        allowed_list.append(word)
+        print("{word} has passed".format(word=word))
+    else:
+        print("{word} has failed".format(word=word))
+
+print("\n\nResults:\n---------------------------\n")
+
+allowed_list.sort(key=len)
+for row in allowed_list:
+    print("Ordered in longest allowed is: {allowed_words} at {length_of_this_word} characters long. "
+          "Shown on a 7-segment-display as {displayed_number}".format(allowed_words=row, length_of_this_word=len(row),
+                                                                      displayed_number=convert_to_calculator_number(row)
+                                                                      ))
 
 
-translated_numbers = []
-for letter in highest_allowed_word:
-    translated_numbers.append(letters_to_numbers[letter.lower()])
-
-translated_numbers.reverse()
-translated_numbers = "".join(translated_numbers)
-
-print("The longest letter you can put on a upside down 7-segment display is: {longest} \n"
+print("\nThe longest letter you can put on a upside down 7-segment display is: {longest} \n"
       "The numbers to put in is: {numbers} \n"
       "The word has {length_of_word} characters"
-      .format(longest=highest_allowed_word, numbers=translated_numbers, length_of_word=len(highest_allowed_word)))
+      .format(longest=highest_allowed_word, numbers=convert_to_calculator_number(highest_allowed_word),
+              length_of_word=len(highest_allowed_word)))
